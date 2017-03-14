bef631cfd1583811d427ca4253d5253b93490b26
Toshihiro2010 <s56122201044@ssru.ac.th>
2017-03-14 16:52:25 +0700

    ทดสอบ Http

--- app/views/setting/login/login-component.ts
+++ app/views/setting/login/login-component.ts
@@ -13,12 +13,28 @@ export class LoginComponent {
     private username : string = "";
     private password : string = "";
     
-    btnLogin(){
+    private btnLogin(){
         let self = this;
         
         console.log("username ===> " , self.username );
         console.log("password ===> " , self.password);
-        http.getJSON("http://192.9.9.252:7080/users").then(function (r) {
+        http.request({ url: "http://192.9.9.112:30/users", method: "GET" }).then(function (response) {
+        //// Argument (response) is HttpResponse!
+        //// Content property of the response is HttpContent!
+        var obj = response.content.toJSON();
+        console.log(JSON.stringify(obj));
+        
+       
+        }, function (e) {
+            //// Argument (e) is Error!
+            console.log(e);
+            
+        });
+        
+        
+    }
+    private btnCheck(){
+        http.getJSON("https://httpbin.org/get").then(function (r) {
             //// Argument (r) is JSON!
             console.log("Sucsess == v");
             
@@ -28,8 +44,7 @@ export class LoginComponent {
             //// Argument (e) is Error
             console.log(e);
         });
-        
-        
+
     }
 
 }
