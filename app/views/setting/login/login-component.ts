4ab925e62bde06e3f16ece4b07e2c67604c81942
Toshihiro2010 <s56122201044@ssru.ac.th>
2017-03-15 11:47:39 +0700

    check status Code  - http

--- app/views/setting/login/login-component.ts
+++ app/views/setting/login/login-component.ts
@@ -3,6 +3,7 @@ import { Users } from "../../../models/users/users";
 
 
 var http = require("http");
+var Toast = require("nativescript-toast");
 
 @Component({
     selector: "Login",
@@ -12,39 +13,75 @@ export class LoginComponent {
 
     private username : string = "";
     private password : string = "";
-    
+
+    private strURL : string = "http://192.9.9.112:30";
+
     private btnLogin(){
         let self = this;
-        
-        console.log("username ===> " , self.username );
-        console.log("password ===> " , self.password);
-        http.request({ url: "http://192.9.9.112:30/users", method: "GET" }).then(function (response) {
-        //// Argument (response) is HttpResponse!
-        //// Content property of the response is HttpContent!
-        var obj = response.content.toJSON();
-        console.log(JSON.stringify(obj));
-        
-       
-        }, function (e) {
-            //// Argument (e) is Error!
-            console.log(e);
-            
-        });
-        
+        var checkEmpty = self.checkEmpty();
+        console.log("empty word  ==>" , checkEmpty);
+        if( checkEmpty == true){
+            http.request({ 
+                url: self.strURL +"/users/"+self.username , 
+                method: "GET" 
+            }).then(function (response) {
+                var statusCode = response.statusCode;
+                if(statusCode != 200){
+                    
+                }else{
+                    var obj = response.content.toJSON();
+                    console.log(JSON.stringify(obj));
+                }
+                console.log("status code => " , statusCode);
+                
+            }, function (e) {//// Argument (e) is Error!
+                console.log("error is " , e);  
+            });
+        }
         
     }
     private btnCheck(){
-        http.getJSON("https://httpbin.org/get").then(function (r) {
-            //// Argument (r) is JSON!
-            console.log("Sucsess == v");
-            
-            console.log(JSON.stringify(r));
-            
-        }, function (e) {
-            //// Argument (e) is Error
-            console.log(e);
+        let self = this;
+        var result;
+        http.request({ 
+                url: self.strURL +"/users/1" , 
+                method: "GET" 
+        }).then(function (response) {
+            var obj = response.content.toJSON();
+            var statusCode = response.statusCode;
+
+            console.log("status code => " , statusCode);
+            console.log(JSON.stringify(obj));
+
+        }, function (e) {//// Argument (e) is Error!
+
+            console.log("error is " , e);  
         });
 
     }
+
+    private httpLogin(){
+        console.log("on => http Login");
+        let self = this;
+
+    }
+
+    private checkEmpty(){
+        let self = this;
+        console.log("username ===> " , self.username );
+        console.log("password ===> " , self.password);
+
+        if(self.username.trim() == "" || self.password.trim() ==""){
+         
+            var toast = Toast.makeText("มีช่องว่างนะไอ้โง่");
+            toast.show();
+            return false;
+        }else{
+            return true;
+        }
+        
+    }
+
+
 
 }
